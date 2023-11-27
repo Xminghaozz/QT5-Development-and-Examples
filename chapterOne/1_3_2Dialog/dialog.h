9aaa15f44fd178bf38e8157bb1fba2d772fbf3ec
Xueminghao <1095870192@qq.com>
2023-11-27 09:44:13 +0800

    20231127

---
+++ chapterOne/1_3_2Dialog/dialog.h
@@ -0,0 +1,26 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+private:
+    QLabel *label1,*label2;
+    QLineEdit *lineEdit;
+    QPushButton *button;
+
+private slots:
+    void showArea();
+};
+
+#endif // DIALOG_H
