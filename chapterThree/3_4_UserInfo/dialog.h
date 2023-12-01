aa2249e4883164f8a8180d72039aef7307dd7a16
Xueminghao <1095870192@qq.com>
2023-12-01 16:37:49 +0800

    20231201-基本布局的练习

---
+++ chapterThree/3_4_UserInfo/dialog.h
@@ -0,0 +1,48 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QTextEdit>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QIcon>
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+private:
+    // 左侧
+    QLabel *UserNameLabel;
+    QLabel *NameLabel;
+    QLabel *SexLabel;
+    QLabel *DepartmentLabel;
+    QLabel *AgeLabel;
+    QLabel *OtherLabel;
+    QLineEdit *UserNameLineEdit;
+    QLineEdit *NameLineEdit;
+    QComboBox *SexComboBox;
+    QTextEdit *DepartmentTextEdit;
+    QLineEdit *AgeLineEdit;
+    QGridLayout *LeftLayout;
+    // 右侧
+    QLabel *HeadLabel;              // 右上角部分
+    QLabel *HeadIconLabel;
+    QPushButton *UpdateHeadBtn;
+    QHBoxLayout *TopRightLayout;
+    QLabel *IntroductionLabel;
+    QTextEdit *IntroductionTextEdit;
+    QVBoxLayout *RightLayout;
+    // 底部
+    QPushButton *OkBtn;
+    QPushButton *CancelBtn;
+    QHBoxLayout *ButtomLayout;
+};
+#endif // DIALOG_H
