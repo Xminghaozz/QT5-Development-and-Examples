bd07601ca6053d54c4d9c2178b5a18b65c17e7b1
Xueminghao <1095870192@qq.com>
2023-12-26 10:09:03 +0800

    2023-12-26 chapter9 9_1_1_TextFile	QFile 类提供了读写文件的接口 使用QFile类读取文件

---
+++ chapter09/9_1_1_TextFile/main.cpp
@@ -0,0 +1,20 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QFile>
+
+int main( int argc, char *argv[] )
+{
+    QCoreApplication a( argc, argv );
+    QFile            file( "textFile1.txt" );
+    if ( file.open( QIODevice::ReadOnly ) )
+    {
+        char   buffer[ 2048 ];
+        qint64 lineLen = file.readLine( buffer, sizeof( buffer ) );
+        if ( lineLen != -1 )
+        {
+            qDebug() << buffer;
+        }
+    }
+
+    return a.exec();
+}
