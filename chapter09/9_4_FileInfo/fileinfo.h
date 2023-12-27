11cb690d8193020c05704e25940254d07e5d2425
Xueminghao <1095870192@qq.com>
2023-12-27 10:43:04 +0800

    2023-12-27 chapter9 9_4_FileInfo QFilelnfo 类提供了对文件进行操作时获得的文件相关属性信息，包括文件名、文件大小、创建时间、最后修改时间、最后访问时间及一些文件是否为目 录、文件或符号链接和读写属性

---
+++ chapter09/9_4_FileInfo/fileinfo.h
@@ -0,0 +1,44 @@
+#ifndef FILEINFO_H
+#define FILEINFO_H
+
+#include <QCheckBox>
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+
+class FileInfo : public QDialog
+{
+    Q_OBJECT
+
+  public:
+    FileInfo( QWidget *parent = nullptr );
+    ~FileInfo();
+
+  public slots:
+    void slotFile();
+    void slotGet();
+
+  private:
+    QLabel      *fileNameLabel;
+    QLineEdit   *fileNameLineEdit;
+    QPushButton *fileBtn;
+    QLabel      *sizeLabel;
+    QLineEdit   *sizeLineEdit;
+    QLabel      *createTimeLabel;
+    QLineEdit   *createTimeLineEdit;
+    QLabel      *lastModifiedLabel;
+    QLineEdit   *lastModifiedLineEdit;
+    QLabel      *lastReadLabel;
+    QLineEdit   *lastReadLineEdit;
+    QLabel      *propertyLabel;
+    QCheckBox   *isDirCheckBox;
+    QCheckBox   *isFileCheckBox;
+    QCheckBox   *isSymLinkCheckBox;
+    QCheckBox   *isHiddenCheckBox;
+    QCheckBox   *isReadableCheckBox;
+    QCheckBox   *isWritableCheckBox;
+    QCheckBox   *isExecutableCheckBox;
+    QPushButton *getBtn;
+};
+#endif // FILEINFO_H
